412ddf80b68b3a69b5c13d523738f8e684f48d4f
Adel Lahlou <adellahlou@users.noreply.github.com>
2015-03-20 14:54:56 -0400

    Put in a friendly warning due to the styling

--- tests.js
+++ tests.js
@@ -9,13 +9,7 @@ $(document).trigger(e3);
 
 
 var testExpressions = ["Result :=  Select[A >= A1 && B != B1](Foo X Rename[B/B1](Rename[A/A1](Foo)))",
-    "Result: = Project[A](Foo) - Project[A]( 
-                       Select[A < A1](
-        Project[A, A1](
-            Foo X(Rename[A / A1](
-                Rename[B / B1](Foo)
-            ))
-        )
-    )
-)"
-];
+                       //selects max of A
+    "Result: = Project[A](Foo) - Project[A]( Select[A < A1](Project[A, A1](Foo X(Rename[A / A1](Rename[B / B1](Foo))))))",
+                       "Result := "
+                      ];
